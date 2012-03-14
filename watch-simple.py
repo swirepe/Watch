e24443bc23ebe7d46989ad9a31ae05a16f1ff2cc
Peter Swire <swirepe@gmail.com>
2012-03-13 21:12:52 -0400

    added examples for globs and support for guessing absolute paths

--- watch-simple.py
+++ watch-simple.py
@@ -1,4 +1,5 @@
-# since there is no easy way to have background processes in windows,
+# Peter Swire, 2012
+# swirepe.com
 # this is basically tail -f, but for arbitrary commands
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 import glob
@@ -28,14 +29,19 @@ class Watcher:
 	def getArguments(self):
 		description = "Run a command when a file changes."
 		epilog = textwrap.dedent('''\
-			example:
+			Examples:
 			    watch --file readme.md --cmd "pandoc readme.md -o readme.html"
+
+			    watch -f somelog.txt -c "cat somelog.txt"
+
+			    watch --file *.c *.h --cmd "make"
 			''')
 		
 		parser = ArgumentParser(prog="watch", formatter_class=RawDescriptionHelpFormatter, description=description, epilog=epilog)
 		parser.add_argument("-f", "--file", action="store",  required=True, nargs='+', help="The file(s) to watch.")
 		parser.add_argument("-c", "--cmd", action="store", required=True, help="The command to run when the file(s) changes.  This should be a quoted string.")
 		parser.add_argument("-i", "--interval", default=POLLING_INTERVAL, action="store", help="The interval to check the file(s) at, in seconds.", type=float)
+		parser.add_argument("-a", "--absolute", action="store_true", help="Guess the absolute paths of the files being watched.")
 		parser.add_argument("-v", "--verbose", action="count", help="Output file names and commands as they are run.")
 		return parser.parse_args()
 
@@ -58,7 +64,14 @@ class Watcher:
 
 			newf.extend(glob.glob(f))
 
-		#files = map(os.path.abspath,newf)
+		files = newf
+		
+		if arguments.absolute:
+			files = map(os.path.abspath,files)
+
+			if self.verbose >= 2:
+				print "[pre] Expanded paths:"
+				print "    " + "\n    ".join(files)
 
 		for f in files:
 			if not os.path.isfile(f):
@@ -105,5 +118,5 @@ if __name__ == "__main__":
 	try:
 		w = Watcher()
 	except KeyboardInterrupt:
-		print "\n\n"
+		print "\n"
 		sys.exit(0)
