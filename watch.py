ec070af9c38735a14b863dadb7d4875ea12ea210
Peter Swire <swirepe@gmail.com>
2012-04-28 09:55:32 -0400

    slight refactoring done

--- watch.py
+++ watch.py
@@ -27,6 +27,8 @@ class Watcher:
 
 
 	def getArguments(self):
+		# note: the --cmd argument needs to be quoted,
+		# so that we don't confuse arguments to this program with arguments to the command to run
 		description = "Run a command when a file changes."
 		epilog = textwrap.dedent('''\
 			Watch: a utility for acting on file changes.
@@ -42,7 +44,7 @@ class Watcher:
 		
 		parser = ArgumentParser(prog="watch", formatter_class=RawDescriptionHelpFormatter, description=description, epilog=epilog)
 		parser.add_argument("-f", "--file", action="store",  required=True, nargs='+', help="The file(s) to watch.")
-		parser.add_argument("-c", "--cmd", action="store", required=True, help="The command to run when the file(s) changes.  This should be a quoted string.")
+		parser.add_argument("-c", "--cmd", action="store", required=True, nargs='+', help="The command to run when the file(s) changes.  This should be a quoted string.")
 		parser.add_argument("-i", "--interval", default=POLLING_INTERVAL, action="store", help="The interval to check the file(s) at, in seconds.", type=float)
 		parser.add_argument("-a", "--absolute", action="store_true", help="Guess the absolute paths of the files being watched.")
 		parser.add_argument("-v", "--verbose", action="count", help="Output file names and commands as they are run.")
@@ -80,9 +82,8 @@ class Watcher:
 			if not (os.path.isfile(f) or os.path.isdir(f)):
 				raise Exception("Error:", f, "doesn't exist")
 
-		cmd = arguments.cmd
-		for f in files:
-			self.watching[f] = cmd
+		self.cmd = shlex.split(arguments.cmd)
+
 
 
 		
@@ -111,7 +112,7 @@ class Watcher:
 							print textwrap.fill(output, initial_indent='', subsequent_indent='    ')
 
 
-						Popen(shlex.split(self.watching[f]))
+						Popen(self.cmd)
 
 
 
