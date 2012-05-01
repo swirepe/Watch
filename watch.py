42ae836c7e7e2f5a1160661a469893cddbea87a0
Peter Swire <swirepe@gmail.com>
2012-05-01 00:21:44 -0400

    started work on recursing directories

--- watch.py
+++ watch.py
@@ -18,12 +18,13 @@ POLLING_INTERVAL = 1
 
 class Watcher:
 	def __init__(self):
-		self.watching = {}
-		self.mod_times = {}
+		self.watching = []
+		self.mod_tracker = {}
 
 		args = self.getArguments()
 		self.formatAndAddArgs(args)
 		self.run()
+
 
 
 	def getArguments(self):
@@ -51,6 +52,7 @@ class Watcher:
 		return parser.parse_args()
 
 
+
 	def formatAndAddArgs(self, arguments):
 		# make sure the file exists
 		# turn the file into an absolute path
@@ -63,10 +65,7 @@ class Watcher:
 		files = arguments.file
 		newf = []
 		for f in files:
-
-			if self.verbose >= 2:
-				print textwrap.fill("[pre] Expanding " + str(f) + " -> " + ", ".join(glob.glob(f)), initial_indent='', subsequent_indent='    ')
-
+			self.verboseGlobExpandReport(f)
 			newf.extend(glob.glob(f))
 
 		files = newf
@@ -75,46 +74,149 @@ class Watcher:
 			files = map(os.path.abspath,files)
 
 			if self.verbose >= 2:
-				print "[pre] Expanded paths:"
+				print "[watch] Expanded paths:"
 				print "    " + "\n    ".join(files)
 
-		for f in files:
-			if not (os.path.isfile(f) or os.path.isdir(f)):
-				raise Exception("Error:", f, "doesn't exist")
-
+		self.getInitialTimes(files)
 		self.cmd = shlex.split(arguments.cmd[0])
 
 
 
-		
-
-
-
 	def run(self):
+		global POLLING_INTERVAL
 		while True:
-			for f in self.watching.keys():
-				# initial case: we don't have a date for these things
-				if not self.mod_times.has_key(f):
-					self.mod_times[f] = os.stat(f).st_mtime
+			time.sleep(POLLING_INTERVAL)
+			for f in self.watching:
+				if self.hasChanged(f):
+					verboseRunReport(f, m)
+					Popen(self.cmd)
+
+
+
+	def getInitialTimes(self, files):
+		for f in files:
+			if not os.path.exists(f):
+				raise Exception("Error:", f, "doesn't exist")
+
+			if os.path.isfile(f):
+				self.mod_tracker[f] = os.stat(f).st_mtime
+			elif os.path.isdir(f):
+				self.addDirToModTracker(f)
+
+		self.verboseStartupReport()
+
+
+	def addDirToModTracker(self, f):
+		if os.path.isdir(f):
+			inf = self.getInDirectory(f)
+			self.mod_tracker[f] = inf
+			self.watching.extend(inf)
+			self.getInitialTimes(inf)
+
+
+	def getInDirectory(self, d):
+		"""recursively go through directories, 
+		listing all of the non-directory files in it"""
+		ind = []
+		for root, dirnames, fnames in os.walk(d):
+			for f in fnames:
+				ind.append( os.path.join(d, f))
+			for d2 in dirnames:
+				ind2 = self.getInDirectory(os.path.join(d,d2))
+				ind.extend(ind2)
+
+		ind.sort()
+
+		return ind
+
+
+
+	def hasChanged(self, f):
+		"""Checks to see if a file or directory has changed,
+		and updates it's status if it has
+
+		If a directory has changed, its contents have changed.
+		If a file has changed, it's modification time has changed.
+		"""
+		# if it doesn't exist, that may be a change. 
+		# We need to remove it from the list of files to watch either way
+		if not os.path.exists(f):
+			self.watching.remove(f)
+
+			if self.mod_tracker.has_key(f):
+				self.mod_tracker.pop(f)
+				return True
+
+			return False
+
+
+		# if it's a directory, the standard st_time won't cut it
+		if os.path.isdir(f):
+			inf = self.getInDirectory(f)
+
+			# if the two contents aren't the same, the 
+			# something has changed
+			if not self.mod_tracker[f] == inf:
+				self.mod_tracker[f] = inf
+				return True
+
+			# if one of the contents has changed, this has changed
+			for fcontent in inf:
+				if self.hasChanged(fcontent):
+					return True
+
+
+		else:
+			# check the hard time for a file
+
+			m = os.stat(f).st_mtime
+			# case 1: the file didn't exist before and does now
+			if not self.mod_tracker.has_key[f]:
+				self.mod_tracker[f] = m
+				self.watching.add(f)
+				return True
+
+			# case 2: the file existed and changed
+			if m > self.mod_tracker[f]:
+				self.mod_tracker[f] = m
+				return True
+
+		return False
+
+
+
+	def verboseGlobExpandReport(self, f):
+		if self.verbose >= 2:
+			print textwrap.fill("[watch] Expanding " + str(f) + " -> " + ", ".join(glob.glob(f)), initial_indent='', subsequent_indent='    ')
+
+
+	def verboseStartupReport(self):
+		if self.verbose >= 3:
+			for f in self.watching:
+				if os.path.isdir(f):
+					print "[watch] Directory " + str(f) + "/ initialized with contents:"
+					for fcontent in self.mod_tracker[f]:
+						print "        " + str(f) + " => " + str(fcontent) 
 				else:
-					m = os.stat(f).st_mtime
-					if m > self.mod_times[f]:
-						self.mod_times[f] = m
-
-						# verbose output stuff
-						if self.verbose >= 1:
-							now = time.gmtime()
-							strnow = "[" + time.strftime("%Y-%m-%d %H:%M:%S", now) + "]"
-
-							output = strnow + " File " + f + " has changed."
-							if self.verbose >= 2:
-								output += "  Running '" + self.watching[f] + "'"
-							print textwrap.fill(output, initial_indent='', subsequent_indent='    ')
+					print "[watch] File " + str(f) + " initialized with time " + str(self.mod_tracker[f])
 
 
-						Popen(self.cmd)
+	def verboseRunReport(self, f, mod_time):
+		# verbose output stuff
+		if self.verbose >= 1:
+			now = time.gmtime()
+			strnow = "[" + time.strftime("%Y-%m-%d %H:%M:%S", now) + "]"
 
+			output = strnow + " File " + f + " has changed."
 
+			if self.verbose >= 3:
+				output += " (from " + str(self.mod_tracker[f]) + " to " + str(mod_time) + ") "				
+
+			if self.verbose >= 2:
+				output += "  Running '" + self.cmd + "'"
+			print textwrap.fill(output, initial_indent='', subsequent_indent='    ')
+
+			
 
 
 
